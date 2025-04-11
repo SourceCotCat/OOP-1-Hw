3e8f980b8aee016e03d0ac50dfd3a162b73a6850
St <valuable_information@mail.ru>
2025-04-11 13:37:18 +0300

    add additional methods

--- main.py
+++ main.py
@@ -7,12 +7,29 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
         
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
         
+    
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -21,21 +38,25 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
+        
+# Новые данные для примера 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
+cool_lecturer = Lecturer('Mike', 'Sally')
+cool_lecturer.courses_attached += ['Python']
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 7)
+cool_reviewer.rate_hw(best_student, 'Python', 6)
+
+best_student.rate_lecturer(cool_lecturer, 'Python', 5)
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
+best_student.rate_lecturer(cool_lecturer, 'Python', 8)
+
 print(best_student.grades)
+print(cool_lecturer.grades)
